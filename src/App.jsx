e67dd9c66889a888d478c413065eab4aa3877afd
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-07-10 05:33:23 -1100

    getting started with tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
-import React, { useState } from 'react'
-import './style/Modal.css'
-import Modal from './components/Modal'
-import buy from './buy.json'
-import receive from './receive.json'
+import React from 'react'
+import '../src/style/Global.css'
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [modalContent, setModalContent] = useState('')
-
-  const openModal = (content) => {
-    setIsModalOpen(true)
-    setModalContent(content)
-  }
-  const closeModal = () => {
-    setIsModalOpen(false)
-  }
-
   return (
     <div>
-      <main className='d-flex header-content p-lg-5'>
-        <header>
-          <h4 className='text-2xl font-bold underline'>Custom Modal</h4>
-        </header>
-        <article className='d-flex header-text'>
-          <p onClick={() => openModal(buy)}>Buy</p>
-          <p className='ms-lg-4' onClick={() => openModal(receive)}>
-            Receive
-          </p>
-        </article>
-      </main>
-      <section>
-        <Modal
-          isModalOpen={isModalOpen}
-          modalContent={modalContent}
-          onClose={closeModal}
-        />
-      </section>
+      <h1 className='text-gray border-1 border-blue sm:p-3'>hello</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,50 @@ export default {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '365px',
+        // => @media (min-width: 365px) { ... }
+
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        xl: '1440px',
+        // => @media (min-width: 1440px) { ... }
+      },
+      spacing: {
+        1: '8px',
+        2: '12px',
+        3: '16px',
+        4: '24px',
+        5: '32px',
+        6: '48px',
+      },
+      colors: {
+        blue: 'red',
+        purple: '#7e5bef',
+        pink: '#ff49db',
+        orange: '#ff7849',
+        green: '#13ce66',
+        yellow: '#ffc82c',
+        gray: '#8492a6',
+        graylight: '#d3dce6',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      borderRadius: {
+        none: '0',
+        sm: '.125rem',
+        DEFAULT: '.25rem',
+        lg: '.5rem',
+        full: '9999px',
+      },
+    },
   },
   plugins: [],
 }
